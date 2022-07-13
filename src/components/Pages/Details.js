007d71f18be6b7a162315b4ac881e81adbb3bac7
Axfez <santimon72@gmail.com>
2022-07-12 23:35:27 -0500

    Navbar Details

--- src/components/Pages/Details.js
+++ src/components/Pages/Details.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 import { useGetIdMutation } from '../../components/Api'
+import { logo } from '../../assets'
 
 export const Details = () => {
   const location = useLocation()
@@ -16,8 +17,18 @@ export const Details = () => {
     getId({ id })
   }
 
-    return (
-        <div>
+  return (
+    <nav className="navbar fixed-top navbar-light bg-light">
+      <Link to="/">
+        <button onClick={() => useGetId()}>Back</button>
+      </Link>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={logo} className="img-fluid" />
+          </div>
         </div>
-    )
+      </div>
+    </nav>
+  )
 }
